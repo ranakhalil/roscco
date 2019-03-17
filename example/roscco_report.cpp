3d467604ad93c5f56085d3b6194c30efaaf72f7e
ranakhalil <rana.ae.khalil@gmail.com>
2019-03-16 21:02:00 -0700

    Sending Steering commands to the kia niro. Tested and works

--- example/roscco_report.cpp
+++ example/roscco_report.cpp
@@ -12,6 +12,9 @@ class RosccoReport
 public:
   RosccoReport();
   void EnableControl(bool enable_control);
+  void SteerTest();
+  // Number of messages to retain when the message queue is full
+  const int QUEUE_SIZE_ = 10;
 
 private:
   ros::NodeHandle nh_;
@@ -20,12 +23,6 @@ private:
   ros::Publisher brake_pub_;
   ros::Publisher steering_pub_;
   ros::Publisher enable_disable_pub_;
-
-  // Number of messages to retain when the message queue is full
-  const int QUEUE_SIZE_ = 10;
-
-  // Store last known value for timed callback
-  bool enabled_ = false;
 };
 
 /**
@@ -52,24 +49,43 @@ void RosccoReport::EnableControl(bool enable_control)
 {
   // gamepad triggers default 0 prior to using them which is 50% for the logitech and xbox controller the initilization
   // is to ensure the triggers have been pulled prior to enabling OSCC command
-  if (enable_control)
-  {
-    roscco::EnableDisable enable_msg;
-    enable_msg.header.stamp = ros::Time::now();
-    enable_msg.enable_control = true;
-    enable_disable_pub_.publish(enable_msg);
-    enabled_ = true;
-  }
-  else
-  {
-    ROS_INFO("Control is not being enabled.");
-  }
+  ROS_INFO("In Enable Control %i", enable_control);
+  roscco::EnableDisable enable_msg;
+  enable_msg.header.stamp = ros::Time::now();
+  enable_msg.enable_control = true;
+  enable_disable_pub_.publish(enable_msg);
+  ROS_INFO("Published Msg");
+}
+
+void RosccoReport::SteerTest()
+{
+    roscco::SteeringCommand steering_msg;
+
+    steering_msg.header.stamp = ros::Time::now();
+    steering_msg.steering_torque = 0.2;
+    steering_pub_.publish(steering_msg);
+
+    steering_msg.header.stamp = ros::Time::now();
+    steering_msg.steering_torque = 0.3;
+    steering_pub_.publish(steering_msg);
+
+    steering_msg.header.stamp = ros::Time::now();
+    steering_msg.steering_torque = 0.4;
+    steering_pub_.publish(steering_msg);
 }
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "roscco_report");
   RosccoReport roscco_report;
-  roscco_report.EnableControl(true);
-  ros::spin();
+  ros::Rate loop_rate_(50);
+  int count = 0;
+  while(ros::ok())
+  {
+    roscco_report.EnableControl(true);
+    roscco_report.SteerTest();
+    ros::spinOnce();
+    loop_rate_.sleep();
+    count++;
+  }
 }
